57e682757bd3bb82fcbe8c33fd809b2572d23460
Khady GUEYE <42519251+kasumi993@users.noreply.github.com>
2025-05-22 23:13:52 +0200

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,6 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
-import axios from 'axios';
 import api from '/lib/api';
 
 // Configure the API URL
